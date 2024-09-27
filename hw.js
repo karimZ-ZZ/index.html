6dae37e84f370b5a9a0dd44524b3b27a06849648
karimZ-ZZ <amrabdullah980@gmail.com>
2024-09-27 20:34:53 +0300

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,10 @@
+var film ={
+    mm : [
+       {Film:"Mission: Impossible",Director:"Christopher McQuarrie",Top_actor:"Tom Cruise"},
+       {Film:"Welad Rizk 3",Director:"Tarek Alarian",Top_actor:" Ahmed Ezz"},
+       {Film:"Ex Meraty",Director:"Moataz El Tony",Top_actor:" Hisham Maged"}
+    ]   
+   }
+   
+    //document.write(film.mm[0].Film)
+
